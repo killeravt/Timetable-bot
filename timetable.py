c0b80a89388a4f6227c7f354f3ad7e3035e036c3
killeravt <alexx.ss3003@gmail.com>
2021-01-26 10:20:19 +0200

    Update timetable.py

--- timetable.py
+++ timetable.py
@@ -2,11 +2,15 @@ import telebot
 import requests
 from bs4 import BeautifulSoup
 from telebot import types
-bot = telebot.TeleBot("1467455929:AAFyqb9WXV5s31HRO3s3DQ1ZpLFR2-kg1sM")
+bot = telebot.TeleBot("1407745525:AAFtQlzk9A99dKX0naKXPNynUAXozY2CrW0")
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-  bot.send_message(message.chat.id, "Для получения ссылки на скачку расписания пропиши: /link")
+  bot.send_message(message.chat.id, "Для получения ссылки на скачку расписания пропиши: /link. Для отображения информации для входа конференций Zoom пропиши: /info")
+
+@bot.message_handler(commands=['help'])
+def help_handler(message):
+  bot.send_message(message.chat.id,"/link - для получения информации, /info - пароли для зума")
 
 @bot.message_handler(commands=['link'])
 def link_handler(message):
@@ -20,4 +24,87 @@ def link_handler(message):
   orig2 = str(orig1).replace('" target="_blank">2 курс (ФЕМ, ФІСТ)</a>', '')
   bot.send_message(message.chat.id, orig2)
 
+@bot.message_handler(commands=['ukr'])
+def ukr_handler(message):
+  bot.send_message(message.chat.id, "Бабінчук Олена Петрівна/Укр.мова")
+  bot.send_message(message.chat.id, "662771 1742")
+  bot.send_message(message.chat.id, "2RHnBx")
+
+@bot.message_handler(commands=['cult'])
+def cult_handler(message):
+  bot.send_message(message.chat.id, "Баликін І.І./Культорологiя")
+  bot.send_message(message.chat.id, "2103939205")
+  bot.send_message(message.chat.id, "671677")
+
+@bot.message_handler(commands=['bezk'])
+def bezk_handler(message):
+  bot.send_message(message.chat.id, "Бескровний Олексій Іванович/Математика за школою")
+  bot.send_message(message.chat.id, "73485639721")
+  bot.send_message(message.chat.id, "016871")
+
+@bot.message_handler(commands=['kotov'])
+def kotov_handler(message):
+  bot.send_message(message.chat.id, "Котвицька Н.М./Економiка,Основи екологiї")
+  bot.send_message(message.chat.id, "265-730-45-66")
+  bot.send_message(message.chat.id, "5Zcve1")
+
+@bot.message_handler(commands=['varenik'])
+def varenik_handler(message):
+  bot.send_message(message.chat.id, "Індентифікатор Вареника Б.В/Фізкультура")
+  bot.send_message(message.chat.id, "7 412 019 970")
+  bot.send_message(message.chat.id, "K96XGw")
+
+@bot.message_handler(commands=['voyna'])
+def voyna_handler(message):
+  bot.send_message(message.chat.id, "Войнаровська Л. І./Філософія")
+  bot.send_message(message.chat.id, "98138858472")
+  bot.send_message(message.chat.id, "mBC3LE")
+
+@bot.message_handler(commands=['giyasov'])
+def giyasov_handler(message):
+  bot.send_message(message.chat.id, "Гіясов Гусейн Аббасович/Спеціальні предмети")
+  bot.send_message(message.chat.id, "7863304882")
+  bot.send_message(message.chat.id, "953197")
+
+@bot.message_handler(commands=['rudn'])
+def rudn_handler(message):
+  bot.send_message(message.chat.id, "Рудницька Л.В./Спеціальні предмети")
+  bot.send_message(message.chat.id, "93463988582")
+  bot.send_message(message.chat.id, "026623")
+
+@bot.message_handler(commands=['milash'])
+def milash_handler(message):
+  bot.send_message(message.chat.id, "Милашенко Віктор Миколайович/Спеціальність")
+  bot.send_message(message.chat.id, "8382329520")
+  bot.send_message(message.chat.id, "12345")
+
+@bot.message_handler(commands=['ukrprof'])
+def ukrprof_handler(message):
+  bot.send_message(message.chat.id, "Кузьманенко А.В./Укр. мова по спеціальності")
+  bot.send_message(message.chat.id, "7238654884")
+  bot.send_message(message.chat.id, "auWT5K")
+
+@bot.message_handler(commands=['fortuna'])
+def fortuna_handler(message):
+  bot.send_message(message.chat.id, "Фортуна Василь Васильович/Висша математика")
+  bot.send_message(message.chat.id, "4684579044")
+  bot.send_message(message.chat.id, "4684579044")
+
+@bot.message_handler(commands=['opolsky'])
+def opolsky_handler(message):
+  bot.send_message(message.chat.id, "Фортуна Василь Васильович/Висша математика")
+  bot.send_message(message.chat.id, "3832414713")
+  bot.send_message(message.chat.id, "12345")
+
+@bot.message_handler(commands=['shyr'])
+def shyr_handler(message):
+  bot.send_message(message.chat.id, "Щур Євгеній Олександрович/Англійська мова")
+  bot.send_message(message.chat.id, "3349349410")
+  bot.send_message(message.chat.id, "426902")
+
+@bot.message_handler(commands=['info'])
+def info_handler(message):
+  bot.send_message(message.chat.id, "Команды для преподов: /ukr - Укр мова, /cult Культорологiя, /bezk Шкільна математика, /kotov Економiка, Основи екологiї, /varenik Фізкультура, /voyna Філософія, /giyasov Програмування, /milash Програмування, /rudn Історія України, /ukrprof Проф. укр. мова, /fortuna Висша математика, /opolsky Фізика, /shyr Англ. мова")
+
 bot.polling(none_stop = True)
+
